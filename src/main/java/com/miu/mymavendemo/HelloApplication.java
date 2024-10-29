3ad56c1f77f3bda1054207ed6b6fc99117d23a7f
Jhonatan <jhonatan_bellaiza@hotmail.com>
2024-10-29 13:09:34 -0400

    Update version

--- src/main/java/com/miu/mymavendemo/HelloApplication.java
+++ src/main/java/com/miu/mymavendemo/HelloApplication.java
@@ -5,16 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello1!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+
     }
 
     public static void main(String[] args) {
